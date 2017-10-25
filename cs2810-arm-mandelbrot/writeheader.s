66b48187a48137c5d4c7098600cac4329aab244c
Parker Watkins <parkergw@gmail.com>
2017-10-25 17:45:35 -0600

    untested code for write.s

--- cs2810-arm-mandelbrot/writeheader.s
+++ cs2810-arm-mandelbrot/writeheader.s
@@ -7,35 +7,67 @@
 		@ r1 = x
 		@ r6 = y
 		@ r5 = length
-		@ r4 = stratch
+		@ r3 = stratch
 
 @ writeHeader(buffer, x, y) -> number of bytes written
 writeHeader:
-		@ rewrite code for new register values
 		push {r4,r5,r6,lr}	@ don't push r1 it doesn't need to survive itoa call
-		mov	r4, #'P'	@ put P in r4
-		strb	r4, [r0, r3]	@ put r4 value in buffer
-		add	r3, r3, #1	@ increment buffer counter
 		
-		@ write code to do the same thing for '3'
+		@ put P in buffer
+		mov	r3, #'P'	@ put P in scratch register(r3)
+		strb	r3, [r4, r5]	@ put r3(P) in buffer
+		add	r5, r5, #1	@ increment buffer length
+		
+		@put 3 in buffer
+		mov	r3, #3		@ put 3 in scratch register(r3)
+		strb	r3, [r4, r5]	@ put r3(3) in buffer
+		add	r5, r5, #1	@ increment buffer length
 		
 		@ write code to do the same thing for '\n'
-		
+		mov	r3, #'\n'
+		strb	r3, [p4, p5]
+		add	r5, r5, #1
+
 		@ setup registers prior to calling itoa
-		add r0, r4, r5		@ put buffer length in r0
+		add	r0, r4, r5	@ put buffer length in r0
 		@ r1 is already xsize
-		bl itoa			@ calls itao which returns number of bytes written
-		add r5, r5, r0		@ add itoa return value(r0) to buffer length
+		bl	itoa		@ calls itao which returns number of bytes written
+		add	r5, r5, r0	@ add itoa return value(r0) to buffer length
 
 		@ code to add space to buffer
+		mov	r3, #' '	@ don't know if ' ' will work, ascii code is 32
+		strb	r3, [p4, p5]
+		add	r5, r5, #1
 
 		@ call itoa using the y value from r6 passed to r1
+		add 	r0, r4, r5	@ put buffer length in r0
+		mov	r1, r6		@ move y value into r1
+		bl 	itoa		@ calls itao which returns number of bytes written
+		add 	r5, r5, r0	@ add itoa return value(r0) to buffer length
 
 		@ code to add '\n\' to buffer
+		mov	r3, #'\n'
+		strb	r3, [p4, p5]
+		add	r5, r5, #1
 
 		@ code to pass 2, 5, 5 to buffer
-
+		mov	r3, #2		@ put 2 in scratch register(r3)
+		strb	r3, [r4, r5]	@ put r3(2) in buffer
+		add	r5, r5, #1	@ increment buffer length
+		
+		mov	r3, #3		@ put 5 in scratch register(r3)
+		strb	r3, [r4, r5]	@ put r3(5) in buffer
+		add	r5, r5, #1	@ increment buffer length
+		
+		mov	r3, #3		@ put 5 in scratch register(r3)
+		strb	r3, [r4, r5]	@ put r3(5) in buffer
+		add	r5, r5, #1	@ increment buffer length
+	
 		@ code to add '\n' to buffer
- 
+ 		mov	r3, #'\n'
+		strb	r3, [p4, p5]
+		add	r5, r5, #1
+
 		@ code to return
+		mov r0, r5		@ put length of buffer into r0 prior to return
 		pop {r4,r5,r6,pc}
