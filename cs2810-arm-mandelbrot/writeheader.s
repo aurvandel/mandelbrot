4c2858993b02565fbd4c2ceeb1ccd0a17d682c36
aurvandel <parkergw@gmail.com>
2017-10-26 15:51:15 -0600

    initialize length of buffer to 0 to fix seg fault

--- cs2810-arm-mandelbrot/writeheader.s
+++ cs2810-arm-mandelbrot/writeheader.s
@@ -11,7 +11,8 @@
 
 @ writeHeader(buffer, x, y) -> number of bytes written
 writeHeader:
-		push {r4,r5,r6,lr}	@ don't push r1 it doesn't need to survive itoa call
+		push	{r4,r5,r6,lr}	@ don't push r1 it doesn't need to survive itoa call
+		mov	r5, r5, #0	@ initialize length to 0
 		
 		@ put P in buffer
 		mov	r2, #'P'	@ put P in scratch register
@@ -20,12 +21,12 @@ writeHeader:
 		
 		@put 3 in buffer
 		mov	r2, #3		@ put 3 in scratch register
-		@strb	r2, [r4, r5]	@ put r3 in buffer
+		strb	r2, [r4, r5]	@ put r3 in buffer
 		add	r5, r5, #1	@ increment buffer length
 		
 		@ write code to do the same thing for '\n'
 		mov	r2, #'\n'
-		@strb	r2, [p4, p5]
+		strb	r2, [p4, p5]
 		add	r5, r5, #1
 
 		@ setup registers prior to calling itoa
@@ -36,7 +37,7 @@ writeHeader:
 
 		@ code to add space to buffer
 		mov	r2, #' '	@ don't know if ' ' will work, ascii code is 32
-		@strb	r2, [p4, p5]
+		strb	r2, [p4, p5]
 		add	r5, r5, #1
 
 		@ call itoa using the y value from r6 passed to r1
@@ -47,27 +48,27 @@ writeHeader:
 
 		@ code to add '\n\' to buffer
 		mov	r2, #'\n'
-		@strb	r2, [p4, p5]
+		strb	r2, [p4, p5]
 		add	r5, r5, #1
 
 		@ code to pass 2, 5, 5 to buffer
 		mov	r2, #2		@ put 2 in scratch register
-		@strb	r2, [r4, r5]	@ put r3 in buffer
+		strb	r2, [r4, r5]	@ put r3 in buffer
 		add	r5, r5, #1	@ increment buffer length
 		
 		mov	r2, #3		@ put 5 in scratch register
-		@strb	r2, [r4, r5]	@ put r3 in buffer
+		strb	r2, [r4, r5]	@ put r3 in buffer
 		add	r5, r5, #1	@ increment buffer length
 		
 		mov	r2, #3		@ put 5 in scratch register
-		@strb	r2, [r4, r5]	@ put r3 in buffer
+		strb	r2, [r4, r5]	@ put r3 in buffer
 		add	r5, r5, #1	@ increment buffer length
 	
 		@ code to add '\n' to buffer
  		mov	r2, #'\n'
-		@strb	r2, [p4, p5]
+		strb	r2, [p4, p5]
 		add	r5, r5, #1
 
 		@ code to return
-		mov r0, r5		@ put length of buffer into r0 prior to return
-		pop {r4,r5,r6,pc}
+		mov	r0, r5		@ put length of buffer into r0 prior to return
+		pop	{r4,r5,r6,pc}
