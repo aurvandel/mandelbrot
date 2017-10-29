be75a225a50d65d57d99de3af1935549e9c3700e
Parker <parkergw@gmail.com>
2017-10-28 19:29:05 -0600

    step 3 done

--- cs2810-arm-mandelbrot/writeheader.s
+++ cs2810-arm-mandelbrot/writeheader.s
@@ -12,8 +12,9 @@
 @ writeHeader(buffer, x, y) -> number of bytes written
 writeHeader:
 		push	{r4,r5,r6,lr}	@ don't push r1 it doesn't need to survive itoa call
-		mov	r5, #0
-		mov	r4, r0
+		mov	r5, #0		@ initilize length to 0
+		mov	r4, r0		@ initilize buffer to r4
+		mov	r6, r2		@ put y value in r6
 	
 		@ put P in buffer
 		mov	r3, #'P'	@ put P in scratch register
@@ -32,7 +33,7 @@ writeHeader:
 
 		@ setup registers prior to calling itoa
 		add    r0, r4, r5     	@ put buffer length in r0
-		r1 is already xsize
+		@r1 is already xsize
 		bl     itoa           	@ calls itao which returns number of bytes written
 		add    r5, r5, r0     	@ add itoa return value to buffer length
 
