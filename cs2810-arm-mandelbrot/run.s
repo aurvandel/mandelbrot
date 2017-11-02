be155ff062927a2c2c786803469b9a313323f828
Parker <parkergw@gmail.com>
2017-11-02 11:38:24 -0600

    wrote sys_open

--- cs2810-arm-mandelbrot/run.s
+++ cs2810-arm-mandelbrot/run.s
@@ -16,9 +16,22 @@
 
 @ run() -> exit code
 run:
+		ldr	r0, =filename
+		ldr	r1, =flags		@fd = open(filename, flags, mode)
+		mov	r2, #mode
+		mov	r7, #sys_open
+		svc	#0
+		
+						@if fd < 0: return 1 (use the constant fail_open)
 
-@ your code goes here
+						@bufsize = writeHeader(buffer, xsize, ysize)
+						@status = write(fd, buffer, bufsize)
+						@if status < 0: return fail_writeheader
 
+						@status = close(fd)
+						@if status < 0: return fail_close
+
+						@return 0 (success)
 
                 .bss
 buffer:         .space 64*1024
