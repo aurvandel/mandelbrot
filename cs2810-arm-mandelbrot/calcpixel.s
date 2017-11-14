237b97e290ad99e57eb370c70a7dfe7f0c8e1e87
Parker <parkergw@gmail.com>
2017-11-14 12:49:40 -0700

    update

--- cs2810-arm-mandelbrot/calcpixel.s
+++ cs2810-arm-mandelbrot/calcpixel.s
@@ -3,21 +3,27 @@
                 .text
 
 		@d3 = 255.0 / 4.0
-		@d4 = x
-		@d5 = y
+		@d0 = x
+		@d1 = y
 
 @ calcPixel(maxiters, col, row) -> rgb
 calcPixel:
 
-		fldd	d3, 255		@setup d3 = 255.0 / 4
+		fldd	d3, constant	@setup d3 = 255.0 / 4
 		fldd	d4, four
 		fdivd	d3, d3, d4
 					@assume 256x256
 					@x = (col - 128) / (255.0 / 4.0)
 		sub	r1, r1, #128	@r1 = col - 128
-		
+		vmov	s10, r1		@put r1 into s10
+		fsitod	d0, s10		@convert s10 to float and put in d0
+		fdivd	d0, d0, d3	
 					@y = -(row - 128) / (255.0 / 4.0)
+		sub	r2, r2, #128
+		vmov	s10, r2
+		fsitod	d1, s10
+		fdivd	d1, d1, d3
 
-255:		.double	255.0
+constant:	.double	255.0
 
 four:		.double	4.0
