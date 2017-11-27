89ee536eaf65b3f77e884fcf76b618e3b9f6ef29
Parker <parkergw@gmail.com>
2017-11-27 13:52:33 -0700

    update

--- cs2810-arm-mandelbrot/getcolor.s
+++ cs2810-arm-mandelbrot/getcolor.s
@@ -24,4 +24,4 @@ getColor:
 		@ use r0 as all three color channels
                 @orr     r1, r0, r0, lsl #8
                 @orr     r0, r1, r0, lsl #16
-                @pop     {ip,pc}
+                pop     {ip,pc}
